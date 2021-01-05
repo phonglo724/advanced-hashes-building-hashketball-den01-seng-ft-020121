e719b4018a64e1c26e4a26265b68cf01d6159935
Phong Lo <phonglo724@gmail.com>
2021-01-05 17:49:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
      team_name: "Brooklyn Nets",
      colors: ["Black","White"],
      players: [{
-       "Alan Andersen" => {
+       "Alan Anderson" => {
        number: 0,
        shoe: 16,
        points: 22,
